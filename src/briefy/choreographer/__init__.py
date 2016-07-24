adb7da236197655e826ddb55838babae040ba555
Érico Andrei <ericof@gmail.com>
2016-07-24 15:57:48 +0200

    Auto load configure.zcml

--- src/briefy/choreographer/__init__.py
+++ src/briefy/choreographer/__init__.py
@@ -1,1 +1,9 @@
 """Briefy Choreographer."""
+from briefy import choreographer
+from zope.configuration.xmlconfig import XMLConfig
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+XMLConfig('configure.zcml', choreographer)()

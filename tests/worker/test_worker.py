57e4260fd1fa3d49ddc6423edc1d4959a276fbeb
Joao S. O. Bueno <gwidion@gmail.com>
2016-08-03 11:11:56 -0300

    :lipstick: Fix formatting

--- tests/worker/test_worker.py
+++ tests/worker/test_worker.py
@@ -27,6 +27,7 @@ class AssertRead(dict):
         for key in self:
             self.read_count[key] = 0
 
+
 class TestQueue:
     def __init__(self):
         self.messages = []
@@ -45,12 +46,13 @@ def message():
             self.body = AssertRead(
                 event_name='dummy.event',
                 guid=uuid.uuid4(),
-                data = {},
+                data={},
                 actor='actor',
                 request_id=uuid.uuid4(),
                 created_at=datetime.utcnow()
             )
             self.body.reset()
+
         def delete(self): pass
 
     return DummyMessage()
@@ -64,7 +66,7 @@ def test_worker_doesnot_process_message_with_missing_fields(notify_mock, query_m
     query_mock.side_effect = lambda interface, name, context: dict
     body = message.body.copy()
     message.body['guid'] = None
-    w  = Worker(TestQueue())
+    w = Worker(TestQueue())
     answer = w.process_message(message)
     assert not answer
     message.body = body
@@ -75,10 +77,13 @@ def test_worker_doesnot_process_message_with_missing_fields(notify_mock, query_m
 
 @mock.patch("briefy.choreographer.worker.queryUtility")
 @mock.patch("briefy.choreographer.worker.notify")
-def test_worker_doesnot_process_message_when_event_factory_not_found(notify_mock, query_mock, message):
+def test_worker_doesnot_process_message_when_event_factory_not_found(
+        notify_mock,
+        query_mock,
+        message):
     query_mock.side_effect = lambda interface, name, context: None
     logger_mock = mock.Mock()
-    w  = Worker(TestQueue(), logger_=logger_mock)
+    w = Worker(TestQueue(), logger_=logger_mock)
     assert not w.process_message(message)
     assert 'has no handler' in logger_mock.info.call_args[0][0]
 
@@ -88,8 +93,8 @@ def test_worker_doesnot_process_message_when_event_factory_not_found(notify_mock
 def test_all_body_parameters_are_used(notify_mock, query_mock, message):
     query_mock.side_effect = lambda interface, name, context: dict
     logger_mock = mock.Mock()
-    w  = Worker(TestQueue(), logger_mock)
-    answer = w.process_message(message)
+    w = Worker(TestQueue(), logger_mock)
+    w.process_message(message)
     notification = notify_mock.call_args[0][0]
     assert isinstance(notification, dict)
     assert all(message.body.read_count.values())
@@ -102,8 +107,10 @@ def test_all_body_parameters_are_used(notify_mock, query_mock, message):
 @mock.patch("briefy.choreographer.worker.Worker")
 @mock.patch("briefy.choreographer.worker.queryUtility")
 def test_worker_main(query_mock, worker_mock):
+
     def worker_call():
         raise RuntimeError
+
     def worker_init(input_queue, logger_, run_interval=.5):
         assert input_queue
         assert logger_
@@ -111,4 +118,3 @@ def test_worker_main(query_mock, worker_mock):
     worker_mock.side_effect = worker_init
     with pytest.raises(RuntimeError):
         main()
-

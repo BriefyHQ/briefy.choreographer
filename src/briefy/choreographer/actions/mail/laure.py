9458612b960fc0122a688fb2ad16bb9ec54cedbe
Joao S. O. Bueno <gwidion@gmail.com>
2017-02-11 15:29:04 -0200

    :lipstick: X 3: The first thing we do: let's kill all the linthers

--- src/briefy/choreographer/actions/mail/laure.py
+++ src/briefy/choreographer/actions/mail/laure.py
@@ -35,7 +35,7 @@ class LaureMail(Mail):
         }
 
     def transform(self) -> dict:
-        '''Add generic fields to the e-mail payload.'''
+        """Add generic fields to the e-mail payload."""
         payload = Objectify(super().transform())
 
         payload.sender_email = MAIL_ACTION_LEICA_SENDER_EMAIL
@@ -69,14 +69,12 @@ class LaureInvalidates(LaureMail):
 
     @property
     def subject(self):
-        '''Generate the e-mail subject.'''
-
+        """Generate the e-mail subject."""
         return ''''{id}' Automatic Check for images failed: re-submission needed!'''.format(
             id=self.assignment_title)
 
     def transform(self)->dict:
-        '''Add the secific fields to the payload.'''
-
+        """Add the secific fields to the payload."""
         payload = Objectify(super().transform())
         data = Objectify(self.data)
 

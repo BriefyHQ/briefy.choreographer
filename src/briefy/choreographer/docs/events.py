714b5763b6a340e27dc25bcb3c12e8aa5378bf34
Érico Andrei <ericof@gmail.com>
2017-04-11 16:38:13 +0200

    Improve event documentation

--- src/briefy/choreographer/docs/events.py
+++ src/briefy/choreographer/docs/events.py
@@ -44,26 +44,27 @@ class EventsDirective(Directive):
         events = self.events
         actions = self.event_actions
         event_names = sorted(events.keys())
-        node = nodes.section(ids=['events'])
+        node = nodes.section(ids=['events'], classes=['events'])
         node += nodes.title(text='Events')
         for event_name in event_names:
             klass = events.get(event_name)
             description = klass.__doc__
             event_actions = actions.get(event_name)
             event_id = 'events-{event_name}'.format(event_name=event_name)
-            event_node = nodes.section(ids=[event_id])
+            event_node = nodes.section(ids=[event_id], classes=['event'])
             event_node += nodes.title(text=event_name)
             event_node += self._rst2node(description)
             if event_actions:
                 actions_id = '{event_id}-actions'.format(event_id=event_id)
-                actions_node = nodes.section(ids=[actions_id])
+                actions_node = nodes.section(ids=[actions_id], classes=['actions'])
                 actions_node += nodes.title(text='Actions')
                 for action_id, action in event_actions.items():
                     action_desc = self._rst2node(action.__doc__)
                     temp = nodes.definition_list_item()
                     temp += nodes.term(text=action_id, ids=[action_id])
-                    temp += nodes.definition()
-                    temp += action_desc
+                    definition = nodes.definition()
+                    definition += action_desc
+                    temp += definition
                     actions_node += temp
                 event_node += actions_node
             node += event_node

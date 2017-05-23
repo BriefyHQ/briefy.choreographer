56d044d2c186b4bd8c36696a9594d284b5e6f784
Érico Andrei <ericof@gmail.com>
2017-05-23 15:15:16 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test_requirements = [
 
 setup(
     name='briefy.choreographer',
-    version='2.0.15',
+    version='2.0.16',
     description='Briefy Choreographer composes actions based on events.',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[

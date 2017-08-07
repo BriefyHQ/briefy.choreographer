0ef308b3c2edd058e72bd6e13892d7dce0dcfc1c
Érico Andrei <ericof@gmail.com>
2017-08-07 16:13:12 +0200

    Back to development

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 setup(
     name='briefy.choreographer',
-    version='2.0.19',
+    version='2.1.0',
     description='Briefy Choreographer composes actions based on events.',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
